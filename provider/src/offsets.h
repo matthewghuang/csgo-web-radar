6d585e49dd5b4a1b43c1b4f1a8a34bbb954c8736
yAIYa <38789201+yAIYa@users.noreply.github.com>
2020-04-29 12:03:37 +0800

    Update offsets.h

--- provider/src/offsets.h
+++ provider/src/offsets.h
@@ -1,17 +1,17 @@
 struct offsets {
-    const static unsigned long client_state = 0xe20d08;
-    const static unsigned long client_state_map = 0x324;
-    const static unsigned long client_state_max_player = 0x420;
-    const static unsigned long entity_list = 0x217b068;
-    const static unsigned long local_player = 0x214af10;
+    const static unsigned long client_state = 0x589DCC;
+    const static unsigned long client_state_map = 0x28C;
+    const static unsigned long client_state_max_player = 0x388;
+    const static unsigned long entity_list = 0x4D43AB4;
+    const static unsigned long local_player = 0xD2FB84;
 };
 
 struct netvars {
-    const static unsigned long health = 0x138;
-    const static unsigned long origin = 0x170;
-    const static unsigned long last_place = 0x3df0;
-    const static unsigned long team = 0x12c;
-    const static unsigned long simulation_time = 0x2a0;
+    const static unsigned long health = 0x100;
+    const static unsigned long origin = 0x138;
+    const static unsigned long last_place = 0x35B4;
+    const static unsigned long team = 0xF4;
+    const static unsigned long simulation_time = 0x268;
 };
 
 extern offsets offset;
